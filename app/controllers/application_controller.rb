67455588055e2622b2f488f1e78a42cc9db8d652
野澤優輔 <nozawayuusuke@nozawayuusukenoMacBook.local>
2018-08-28 13:11:46 +0900

    ユーザー編集フォーム：クラス名変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,6 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
     end
 
